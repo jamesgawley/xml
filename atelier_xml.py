74cc084625473a54a1b3b60bcdc6a869eb18e866
jamesgawley <james.gawley@gmail.com>
2021-12-15 16:59:19 +0100

    Update py file

--- atelier_xml.py
+++ atelier_xml.py
@@ -5,6 +5,37 @@ for (name, member) in getmembers(ET, isfunction):
         if not name.startswith("_"):
             print(name)
 
+# parse the document
+tree = ET.parse("xml/simple.xml")
 
+#manipulate the root element
 root = tree.getroot()
+# print(ET.tostring(root))
+
+#create a new element from text
+elementobj = ET.fromstring("<name>Mr. Awesome</name>")
+
+#add new element to the DOM
 root.append(elementobj)
+
+#write the new XML file
+tree.write("doc.xml")
+
+#alternate way of adding tags:
+
+#create element without text
+elementobj = ET.Element("tag")
+elementobj.text = "text"
+root.append(elementobj)
+
+#find an element with a particular attribute
+foundobj = tree.find(".//tagname[@n='value']")
+
+#find an element with a particular text value
+foundobj = tree.find("[name='Mr. Awesome']")
+
+# look at an attribute
+elementobj.attrib['attrib_name']
+
+# remove an element
+foundobj = 
